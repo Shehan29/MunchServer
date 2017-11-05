d08235858f2dfa4889c7438b048d40507cd6fe25
shehan29 <shehan29@hotmail.com>
2017-11-05 01:52:43 -0400

    Server page

--- server.js
+++ server.js
@@ -6,70 +6,92 @@ const clarifaiApp = new Clarifai.App({
 	apiKey: 'fd87ab258b3349538f837a7cb36886bb'
 });
 
-const http = require('http');
+const http = require('http'),
+	fs = require('fs');
 
 const NF = new NutritionFacts.default("MVC7zkXutJjLnZnjXSnd2W6XE3nzjU0tWAzVDvii");
 
-server = http.createServer(function (request, response) {
-	response.setHeader('Access-Control-Allow-Origin', '*');
-	response.setHeader('Access-Control-Request-Method', '*');
-	response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
-	response.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
-	
-	if (request.method == 'POST') {
-		var body = '';
-		
-		request.on('data', function (p_data) {
-			body += p_data;
-		});
-		request.on('end', function () {
-			
-			if (request.url == '/recognize') {
-				console.log("Sending image to API ...");
-				
-				clarifaiApp.models.predict(Clarifai.FOOD_MODEL, {base64: decodeURIComponent(JSON.parse(body).img)}, {minValue: 0.8, maxConcepts: 5}).then(
-					function (imgResponse) {
-						const matchedList = {
-							"images": imgResponse.outputs[0].data.concepts.map(function (item) {
-								return item['name'];
-							})
-						}
-						console.log(matchedList);
-						response.end(JSON.stringify(matchedList));
-					},
-					function (e) {
-						console.log("Error: " + e.message);
-					}
-				);
-			}
-			else if (request.url == '/data') {
-				NF.searchFoods({
-					q: body,
-					ds: 'Standard Reference',
-					max: 1
-				}).then(function(results) {
-					var foodItem = results.list.item[0];
 
-					foodItem.getNutrition().then(function(nutritionReport) {
-						console.log(nutritionReport);
-						response.end(JSON.stringify(nutritionReport.nutrients));
-					}).catch(function(err) { console.log(err) })
-				}).catch(function(err) { console.log(err) });
-			}
-			else { console.log("Endpoint unavailable")}
-		});
+fs.readFile('./index.html', function (err, html) {
+	if (err) {
+		throw err;
 	}
-	else { response.end(); }
+	server = http.createServer(function (request, response) {
+		response.setHeader('Access-Control-Allow-Origin', '*');
+		response.setHeader('Access-Control-Request-Method', '*');
+		response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
+		response.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+		
+		if (request.url == '/index') {
+			response.write(html);
+			response.end();
+		}
+		
+		if (request.method == 'POST') {
+			var body = '';
+			
+			request.on('data', function (p_data) {
+				body += p_data;
+			});
+			request.on('end', function () {
+				
+				if (request.url == '/recognize') {
+					console.log("Sending image to API ...");
+					
+					clarifaiApp.models.predict(Clarifai.FOOD_MODEL, {base64: decodeURIComponent(JSON.parse(body).img)}, {
+						minValue: 0.8,
+						maxConcepts: 5
+					}).then(
+						function (imgResponse) {
+							const matchedList = {
+								"images": imgResponse.outputs[0].data.concepts.map(function (item) {
+									return item['name'];
+								})
+							}
+							console.log(matchedList);
+							response.end(JSON.stringify(matchedList));
+						},
+						function (e) {
+							console.log("Error: " + e.message);
+						}
+					);
+				}
+				else if (request.url == '/data') {
+					NF.searchFoods({
+						q: body,
+						ds: 'Standard Reference',
+						max: 1
+					}).then(function (results) {
+						var foodItem = results.list.item[0];
+						
+						foodItem.getNutrition().then(function (nutritionReport) {
+							console.log(nutritionReport);
+							response.end(JSON.stringify(nutritionReport.nutrients));
+						}).catch(function (err) {
+							console.log(err)
+						})
+					}).catch(function (err) {
+						console.log(err)
+					});
+				}
+				else {
+					console.log("Endpoint unavailable")
+				}
+			});
+		}
+		else {
+			response.end();
+		}
+	});
+	
+	port = 3000;
+	//host = '172.25.150.65';
+	//host = '192.168.11.119';
+	//host = '192.168.137.9';
+	host = '192.168.137.70';
+	server.listen(port, host);
+	console.log('Listening at http://' + host + ':' + port);
 });
-
-port = 3000;
-//host = '172.25.150.65';
-//host = '192.168.11.119';
-//host = '192.168.137.9';
-//host = '192.168.137.70';
-host = 'ec2-18-216-167-111';
-server.listen(port, host);
-console.log('Listening at http://' + host + ':' + port);
 
 
 
